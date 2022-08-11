12b8490cf425d0ec68bf053343e93f13c6ff5889
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-08-11 19:45:08 +0300

    Add solution for 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+console.log(galleryContainer);
+
+const createGalleryCardEl = ({ preview, original, description }) =>
+  `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+
+const addCardGalleryEl = galleryItems.map(createGalleryCardEl).join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', addCardGalleryEl);
+
+galleryContainer.addEventListener('click', evt => {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const selectedCard = evt.target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedCard}" width="800" height="600">
+`);
+
+  instance.show();
+});
